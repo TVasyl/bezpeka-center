c3d63f1f5056198000fffb59501df6ec64220a8b
Vasyl Trots <vaska.trots@gmail.com>
2023-04-12 17:03:19 +0300

    restructured code and add "security.example.html"

--- src/js/app.js
+++ src/js/app.js
@@ -66,29 +66,32 @@ const callMeBtn = document.querySelector(".call-me");
 const callMeForm = document.querySelector(".page__feedback");
 
 
-callMeBtn.addEventListener("click", () => {
-  callMeForm.classList.toggle("show-form");
-});
+// callMeBtn.addEventListener("click", () => {
+//   callMeForm.classList.toggle("show-form");
+// });
 
 
 
 // Show text on the service items //
 const itemService = document.querySelector(".service__items");
 
-itemService.addEventListener("click", (e) => {
+if (itemService) {
+  itemService.addEventListener("click", (e) => {
   e.preventDefault();  
   
   if (e.target.classList.contains("chevron")) {
-    e.target.closest('.item-service__about').classList.toggle("show-text");
-    e.target.closest(".item-service__open").classList.toggle("chevron_rotate");
-  }
-
-    if (e.target.classList.contains("item-service__about")) {
       e.target.closest('.item-service__about').classList.toggle("show-text");
       e.target.closest(".item-service__open").classList.toggle("chevron_rotate");
     }
-  }
-);
+  
+      if (e.target.classList.contains("item-service__about")) {
+        e.target.closest('.item-service__about').classList.toggle("show-text");
+        e.target.closest(".item-service__open").classList.toggle("chevron_rotate");
+      }
+    }
+  );
+}
+
 
 flsFunctions.isWebp();
 
