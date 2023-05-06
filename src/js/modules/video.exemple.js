e684a13fc17e02eabde481dcbf4ceaab6d7c87f6
Vasyl Trots <vaska.trots@gmail.com>
2023-05-06 12:53:29 +0300

    add json files to video exemple

--- src/js/modules/video.exemple.js
+++ src/js/modules/video.exemple.js
@@ -1,6 +1,118 @@
 // import { videoExempleData } from "../data";
-import videoExempleData from "../video.data.json";
-import protectionExempleData from "../protection.data.json";
+// import videoExempleData from "../video.data.json";
+// import protectionExempleData from "../protection.data.json";
+const videoExempleData = [
+    {
+        id: "01",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 1-кімнатної квартири на першому поверсі",
+        cost: 7000,
+    },
+    {
+        id: "02",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 2-кімнатної квартири на першому поверсі",
+        cost: 9000,
+    },
+    {
+        id: "03",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 3-кімнатної квартири на першому поверсі",
+        cost: 25000,
+    },
+    {
+        id: "04",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 4-кімнатної квартири на першому поверсі",
+        cost: 7000,
+    },
+    {
+        id: "05",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 1-кімнатної квартири на першому поверсі",
+        cost: 15000,
+    },
+    {
+        id: "06",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 2-кімнатної квартири на першому поверсі",
+        cost: 15600,
+    },
+    {
+        id: "07",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 3-кімнатної квартири на першому поверсі",
+        cost: 12000,
+    },
+    {
+        id: "08",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 4-кімнатної квартири на першому поверсі",
+        cost: 6000,
+    },
+    {
+        id: "09",
+        img: "img/video_service/example-1.jpg",
+        title: "Відеонагляд для 1-кімнатної квартири на першому поверсі",
+        cost: 23500,
+    },
+];
+const protectionExempleData = [
+    {
+        id: "01",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 7000,
+    },
+    {
+        id: "02",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 9000,
+    },
+    {
+        id: "03",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 25000,
+    },
+    {
+        id: "04",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 7000,
+    },
+    {
+        id: "05",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 15000,
+    },
+    {
+        id: "06",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 15600,
+    },
+    {
+        id: "07",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 12000,
+    },
+    {
+        id: "08",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 6000,
+    },
+    {
+        id: "09",
+        img: "img/video_service/example-1.jpg",
+        title: "Охоронна система для 1-кімнатної квартири на першому поверсі",
+        cost: 23500,
+    },
+];
 
 const itemPlace = document.querySelector(".security-example__items");
 
